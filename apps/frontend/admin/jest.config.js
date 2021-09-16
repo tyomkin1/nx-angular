a3f4e2df45607a961f743479daf6cff70d1ea5cd
tyomkina <tyomkina@mydigicode.com>
2021-09-16 13:25:10 +0300

    feat(nx.json*.eslintrc.json*angular.json*admin-project init): add tags
    * add tags
    * add admin project
    
    #2

--- apps/frontend/admin/jest.config.js
+++ apps/frontend/admin/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'frontend-cliet',
+  displayName: 'frontend-admin',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/apps/frontend/cliet',
+  coverageDirectory: '../../../coverage/apps/frontend/admin',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
